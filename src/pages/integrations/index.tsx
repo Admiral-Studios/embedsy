11c3196b7178f8b48888f7822b34f57179a5a20c
Vitaly <itassan6@gmail.com>
2025-03-04 18:06:27 +0200

    microsoft teams integrations

--- src/pages/integrations/index.tsx
+++ src/pages/integrations/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import { useState, useRef } from 'react'
 import Grid from '@mui/material/Grid'
 
 import Nango from '@nangohq/frontend'
@@ -8,40 +8,69 @@ import { RolesContextProvider } from 'src/context/RolesContext'
 import { SubjectTypes } from 'src/types/acl/subjectTypes'
 import { useAuth } from 'src/hooks/useAuth'
 import axios from 'axios'
+import { Button, Card, CardContent } from '@mui/material'
+import toast from 'react-hot-toast'
 
 const Integrations = () => {
+  const [isMicrosoftTeamsConnected, setIsMicrosoftTeamsConnected] = useState(false)
   const { hasAdminPrivileges } = useAuth()
   const connectUI = useRef<ConnectUI>()
   const nango = new Nango()
-
-  connectUI.current = nango.openConnectUI({
-    onEvent: event => {
-      if (event.type === 'close') {
-        // Handle modal closed.
-      } else if (event.type === 'connect') {
-        // Handle auth flow successful.
-      }
-    }
-  })
 
   const fetchNangoSessionToken = async () => {
     try {
       const response = await axios.post('/api/nango/session_token', {})
 
-      console.log(response.data)
       connectUI.current!.setSessionToken(response.data)
-    } catch (error) {}
+    } catch (error) {
+      console.error(error)
+    }
   }
 
-  useEffect(() => {
+  const getUnits = async () => {
+    try {
+      const resp = await axios.get('/api/nango/get_units')
+
+      console.log(resp.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleAddMicrosoftTeams = () => {
     fetchNangoSessionToken()
-  }, [fetchNangoSessionToken])
+
+    connectUI.current = nango.openConnectUI({
+      onEvent: event => {
+        if (event.type === 'connect') {
+          toast.success('Connected to Microsoft Teams')
+          setIsMicrosoftTeamsConnected(true)
+          getUnits()
+        }
+      }
+    })
+  }
 
   return (
-    // connectUI.current
     <RolesContextProvider>
-      <Grid container spacing={6}>
+      <Grid container item spacing={6}>
         {hasAdminPrivileges && <div></div>}
+
+        <Grid item xs={12}>
+          <Card>
+            <CardContent sx={{ display: 'flex', justifyContent: 'space-between' }}>
+              <h3 style={{ margin: '5px 0' }}>Microsoft Teams</h3>
+
+              {isMicrosoftTeamsConnected ? (
+                <Button>Connected</Button>
+              ) : (
+                <Button variant='contained' onClick={handleAddMicrosoftTeams}>
+                  Connect
+                </Button>
+              )}
+            </CardContent>
+          </Card>
+        </Grid>
       </Grid>
     </RolesContextProvider>
   )
